5e6114b019b6ad4609aa1ac8648a9e9c58c26ebd
bojjibr <paulobojji@gmail.com>
2023-01-23 21:40:29 -0300

    test: unit test PreparationTime

--- gorgeous_lasagna/gorgeous_lasagna_test.go
+++ gorgeous_lasagna/gorgeous_lasagna_test.go
@@ -11,3 +11,13 @@ func TestRemainingOvenTime(t *testing.T) {
 	}
 
 }
+
+func TestPreparationTime(t *testing.T) {
+	got := PreparationTime(2)
+	expected := 4
+
+	if got != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, got)
+	}
+
+}
